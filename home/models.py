96f864ffa75f051b1285a1b599d8a3243f4e7864
chuangying2017 <631711487@qq.com>
2019-12-16 18:20:24 +0800

    上传模型

--- home/models.py
+++ home/models.py
@@ -7,8 +7,8 @@ class ImagesModel(models.Model):
     img_url = models.URLField(verbose_name='图片地址', null=True, blank=True)
     img_path = models.CharField(max_length=255, verbose_name='图片路径', null=True, blank=True)
     name = models.CharField(max_length=255, verbose_name='图片名称', null=True, blank=True)
-    status = models.CharField(choices=[('enabled', '启用'), ('disabled', '禁用')], default='enabled')
-    type1 = models.CharField(choices=[('banner', '轮播'), ('show', '首图')], default='show', db_column='type')
+    status = models.CharField(choices=[('enabled', '启用'), ('disabled', '禁用')], default='enabled', max_length=50)
+    type1 = models.CharField(choices=[('banner', '轮播'), ('show', '首图')], default='show', db_column='type', max_length=50)
     description = models.CharField(max_length=500, verbose_name='详情', null=True, blank=True)
     create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     update_time = models.DateTimeField(verbose_name='更新时间', null=True, blank=True)
@@ -16,5 +16,6 @@ class ImagesModel(models.Model):
     class Meta:
         db_table = 'images'
 
-class ImagesManyModel(models.Model):
+
+class ProductModel(models.Model):
     pass
