ded447126258ff190fde550e5066addec212ff1a
chuangying2017 <631711487@qq.com>
2019-12-16 18:35:48 +0800

    添加模型

--- home/models.py
+++ home/models.py
@@ -3,6 +3,25 @@ from django.db import models
 # Create your models here.
 
 
+class CategoryModel(models.Model):
+    name = models.CharField(verbose_name='分类名称', null=True, blank=True, max_length=100)
+    img_path = models.CharField(verbose_name='分类图标', null=True, blank=True, max_length=255)
+    create_time = models.DateTimeField(verbose_name='创建时间', null=True, blank=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', null=True, blank=True)
+
+
+class BannerModel(models.Model):
+    img_path = models.CharField(verbose_name='图片路径', null=True, blank=True, max_length=255)
+    img_url = models.URLField(verbose_name='图片url', null=True, blank=True, max_length=255)
+    name = models.CharField(verbose_name='名称', null=True, blank=True, max_length=200)
+    create_time = models.DateTimeField(verbose_name='创建时间', null=True, blank=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', null=True, blank=True)
+
+
+class ProductModel(models.Model):
+    pass
+
+
 class ImagesModel(models.Model):
     img_url = models.URLField(verbose_name='图片地址', null=True, blank=True)
     img_path = models.CharField(max_length=255, verbose_name='图片路径', null=True, blank=True)
@@ -17,5 +36,4 @@ class ImagesModel(models.Model):
         db_table = 'images'
 
 
-class ProductModel(models.Model):
-    pass
+
